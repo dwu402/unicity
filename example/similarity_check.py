56f3200cbc725bc94b97b48efc7860f3da415cef
David Dempsey <d.dempsey@auckland.ac.nz>
2019-08-29 16:04:17 +1200

    partly implemented similarity check for prior projects

--- example/similarity_check.py
+++ example/similarity_check.py
@@ -19,7 +19,19 @@ def similarity_check_with_template():
     # plot anonymised similarity check
     proj.similarity_report(comp, client = 'anon', save = 'report_w_template.png')
 
+def similarity_check_prior_project():
+    # load a Project
+    proj = Project('example_project.zip', expecting=['functions.py'])
+    # load a previous Project
+    prior_proj = Project('prior_project.zip', expecting=['functions.py'])
+    # run similarity check on whole file, using template file and 2 cpus
+    comp = proj.compare('functions.py/NetworkNZ.read_network', prior_project=prior_proj)
+    # plot anonymised similarity check
+    proj.similarity_report(comp)
+
 if __name__ == "__main__":
-    similarity_check()
+    #similarity_check()
     
     #similarity_check_with_template()
+
+    similarity_check_prior_project()
