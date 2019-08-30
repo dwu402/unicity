57e097770ad04208126a900561effef3fa2ebe0c
David Dempsey <d.dempsey@auckland.ac.nz>
2019-08-30 16:05:38 +1200

    tidying up docstrings

--- example/dump_docstrings.py
+++ example/dump_docstrings.py
@@ -8,7 +8,7 @@ def dump_docstrings():
     proj = Project('example_project.zip', expecting=['functions.py'])
     
     # output docstrings
-    proj.dump_docstrings('function.py/NetworkNZ.read_network', save = 'read_network.py')
+    proj.dump_docstrings('functions.py/NetworkNZ.read_network', save = 'read_network.py')
 
 if __name__ == "__main__":
     dump_docstrings()
